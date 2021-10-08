e3e1e7351943b87373bd0bba4cd3cd8034e19f46
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-08 16:18:52 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/tg_bots_disccurssions"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Developers', url='https://t.me/tg_bots_disccurssions'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/Lallu-lallus/auto-filter-v2.03')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/tg_bots_disccurssions')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
