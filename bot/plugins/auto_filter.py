f9122157cc7caea8f1dce11b5fd8a379bd48d230
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-08 19:45:54 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -83,7 +83,7 @@ async def auto_filter(bot: Client, update: Message):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"💕{file_size}🚀  {file_name}"
+            button_text = f"📽️{file_size}🚀  {file_name}"
             
             
             if file_type == "video":
